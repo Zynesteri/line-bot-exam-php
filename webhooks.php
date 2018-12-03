17d9f5a151112d8e8ccf0ac42d4a1987facf2d6a
Zynesteri <Zynesteri@gmail.com>
2018-12-04 00:34:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'blSwUUG0DWeMj6W3RW0EjUE3EaUVzpK3UrdNxSWWFvhg3NsEOZb3azYpSjfrp91CPhrCr3Ic0e6483iLs1ksfRL5jnopXbd3I4r0TNmCNB9PtvftoQusuqtmKeQ+HpX3dL58Z8JirPElX8mbQGHvLVGUYhWQfeY8sLGRXgo3xvw=';
 
 // Get POST body content
 $content = file_get_contents('php://input');

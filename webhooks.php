f522826cecc1e5f7ec11b043166624a773507193
Zynesteri <Zynesteri@gmail.com>
2018-12-04 11:26:02 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -15,8 +15,8 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['groupId'];
+			// Get text sent groupId userId
+			$text = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
